068fdf5cdf44bb2b58ee1b021bd7decdce04a0b1
Evandro-Ladislau <evandroladislau23@gmail.com>
2023-07-11 23:30:15 -0300

    :construction: Implementacao das classes principais e inicio dos metodos da classe FXMLDocumentController

--- src/conversor/FXMLDocumentController.java
+++ src/conversor/FXMLDocumentController.java
@@ -4,12 +4,15 @@
  */
 package conversor;
 
+
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -18,12 +21,19 @@ import javafx.scene.control.Label;
 public class FXMLDocumentController implements Initializable {
     
     @FXML
-    private Label label;
+    private Label lblMostrarValorAConverter;
+
+    @FXML
+    private Button btnClick;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private TextField txtValorAConverter;
+    
+    @FXML
+    void btnConverter(ActionEvent event) {
+
+        String nome = txtValorAConverter.getText();
+        lblMostrarValorAConverter.setText("valor: " + nome);
     }
     
     @Override
@@ -31,4 +41,7 @@ public class FXMLDocumentController implements Initializable {
         // TODO
     }    
     
+    
+    
+   
 }
---
+++ src/conversor/model/Moeda.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conversor.model;
+
+import conversor.Conversao;
+
+/**
+ *
+ * @author evandro
+ */
+public class Moeda {
+    
+   private String nome;
+   private double cotacao;
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getCotacao() {
+        return this.cotacao;
+    }
+
+    public void setCotacao(double cotacao) {
+        this.cotacao = cotacao;
+    }
+   
+   
+   
+}
